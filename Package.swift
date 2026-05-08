4c85d27a1620706e60b972fc6e6c1ddf3083bd1d
VPNUK <danny@vpnuk.info>
2026-05-08 23:30:02 +0100

    fix: downgrade swift-tools-version to 5.7 for Xcode 14.2; tvOS v17→v16

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.8
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [
         .iOS(.v15),
         .macOS(.v12),
-        .tvOS(.v17)
+        .tvOS(.v16)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
