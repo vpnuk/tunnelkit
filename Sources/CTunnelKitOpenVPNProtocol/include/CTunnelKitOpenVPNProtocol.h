ad5bb16d61373097fc6992d376d411e115fe7520
VPNUK <danny@vpnuk.info>
2026-05-13 18:18:27 +0100

    Add CTunnelKitOpenVPNProtocol.h umbrella header

---
+++ Sources/CTunnelKitOpenVPNProtocol/include/CTunnelKitOpenVPNProtocol.h
@@ -0,0 +1,22 @@
+#ifndef CTunnelKitOpenVPNProtocol_h
+#define CTunnelKitOpenVPNProtocol_h
+
+#import "CryptoMacros.h"
+#import "PacketMacros.h"
+#import "Allocation.h"
+#import "ZeroingData.h"
+#import "ReplayProtector.h"
+#import "DataPath.h"
+#import "DataPathCrypto.h"
+#import "TLSBox.h"
+#import "Crypto.h"
+#import "CryptoAEAD.h"
+#import "CryptoCBC.h"
+#import "CryptoCTR.h"
+#import "CryptoBox.h"
+#import "MSS.h"
+#import "PacketStream.h"
+#import "ControlPacket.h"
+#import "XOR.h"
+
+#endif /* CTunnelKitOpenVPNProtocol_h */
