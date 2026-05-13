e0435297d6553cf2f2ebbfa3e4c5e614f107aa02
VPNUK <danny@vpnuk.info>
2026-05-13 22:00:20 +0100

    Revert ZeroingData to forward decl (conflicts with Swift class)

--- TunnelKitBridge.h
+++ TunnelKitBridge.h
@@ -199,29 +199,7 @@ size_t safe_crypto_capacity(size_t size, size_t overhead);
 - (nullable NSData *)decompressedDataWithBytes:(const uint8_t *)bytes length:(NSInteger)length error:(NSError **)error;
 @end
 
-@interface ZeroingData : NSObject
-@property (nonatomic, readonly) const uint8_t *bytes;
-@property (nonatomic, readonly) uint8_t *mutableBytes;
-@property (nonatomic, readonly) NSInteger count;
-- (instancetype)initWithCount:(NSInteger)count;
-- (instancetype)initWithBytes:(nullable const uint8_t *)bytes count:(NSInteger)count;
-- (instancetype)initWithUInt8:(uint8_t)uint8;
-- (instancetype)initWithUInt16:(uint16_t)uint16;
-- (instancetype)initWithData:(NSData *)data;
-- (instancetype)initWithData:(NSData *)data offset:(NSInteger)offset count:(NSInteger)count;
-- (instancetype)initWithString:(NSString *)string nullTerminated:(BOOL)nullTerminated;
-- (void)appendData:(ZeroingData *)other;
-- (void)removeUntilOffset:(NSInteger)until;
-- (void)zero;
-- (ZeroingData *)appendingData:(ZeroingData *)other;
-- (ZeroingData *)withOffset:(NSInteger)offset count:(NSInteger)count;
-- (uint16_t)UInt16ValueFromOffset:(NSInteger)from;
-- (uint16_t)networkUInt16ValueFromOffset:(NSInteger)from;
-- (nullable NSString *)nullTerminatedStringFromOffset:(NSInteger)from;
-- (BOOL)isEqualToData:(NSData *)data;
-- (NSData *)toData;
-- (NSString *)toHex;
-@end
+@class ZeroingData;  // defined as Swift class in ZeroingDataSwift.swift
 
 @interface LZOFactory : NSObject
 + (BOOL)isSupported;
