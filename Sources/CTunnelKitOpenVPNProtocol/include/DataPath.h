01098550647aed8f1c8ef7bcefb4e2b2e4908e69
VPNUK <danny@vpnuk.info>
2026-05-13 11:25:32 +0100

    Replace @import CTunnelKitOpenVPNCore with direct header imports in DataPath.h

--- Sources/CTunnelKitOpenVPNProtocol/include/DataPath.h
+++ Sources/CTunnelKitOpenVPNProtocol/include/DataPath.h
@@ -35,8 +35,8 @@
 //
 
 #import <Foundation/Foundation.h>
-
-@import CTunnelKitOpenVPNCore;
+#import "CompressionFramingNative.h"
+#import "CompressionAlgorithmNative.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
