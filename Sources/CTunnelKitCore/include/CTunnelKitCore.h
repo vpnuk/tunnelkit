d9fdc8cfaeeb175279de8015458625dfa0898667
VPNUK <danny@vpnuk.info>
2026-05-13 18:17:33 +0100

    Add CTunnelKitCore.h umbrella header for SPM module resolution

---
+++ Sources/CTunnelKitCore/include/CTunnelKitCore.h
@@ -0,0 +1,9 @@
+#ifndef CTunnelKitCore_h
+#define CTunnelKitCore_h
+
+#import "Allocation.h"
+#import "CompressionProvider.h"
+#import "LZOFactory.h"
+#import "ZeroingData.h"
+
+#endif /* CTunnelKitCore_h */
