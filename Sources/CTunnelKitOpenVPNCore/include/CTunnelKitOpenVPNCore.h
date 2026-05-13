c6aea6f37e7a0118587ae0712b3eadf6c0bd4e60
VPNUK <danny@vpnuk.info>
2026-05-13 18:18:19 +0100

    Add CTunnelKitOpenVPNCore.h umbrella header for SPM module resolution

---
+++ Sources/CTunnelKitOpenVPNCore/include/CTunnelKitOpenVPNCore.h
@@ -0,0 +1,9 @@
+#ifndef CTunnelKitOpenVPNCore_h
+#define CTunnelKitOpenVPNCore_h
+
+#import "CompressionAlgorithmNative.h"
+#import "CompressionFramingNative.h"
+#import "Errors.h"
+#import "XORMethodNative.h"
+
+#endif /* CTunnelKitOpenVPNCore_h */
